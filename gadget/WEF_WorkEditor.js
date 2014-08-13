bd78cc918963df052d07d1e2a7b7360b9050deec
Sergey Vladimirov <vlsergey@gmail.com>
2014-08-13 16:33:27 +0400

    refactor code to use resource loader

--- gadget/WEF_WorkEditor.js
+++ gadget/WEF_WorkEditor.js
@@ -5,15 +5,18 @@
  * @see https://github.com/vlsergey/WE-Framework
  * @author vlsergey
  */
-mw.loader.addSource( {
-	"ruwiki": {
-		"loadScript": "//bits.wikimedia.org/ru.wikipedia.org/load.php",
-		"apiScript": "//ru.wikipedia.org/w/api.php"
-	}
-} );
+try {
+	mw.loader.addSource( {
+		"ruwiki": {
+			"loadScript": "//bits.wikimedia.org/ru.wikipedia.org/load.php",
+			"apiScript": "//ru.wikipedia.org/w/api.php"
+		}
+	} );
+	mw.loader.register( 'ext.gadget.wefcore', 64775771, [ 'jquery.ui.autocomplete', 'jquery.ui.dialog', 'jquery.ui.datepicker', 'jquery.ui.tabs' ], undefined, 'ruwiki' );
+	mw.loader.register( 'ext.gadget.wefflags', 64774333, undefined, undefined, 'ruwiki' );
+} catch ( error ) {
+	// already registered
+}
 
-mw.loader.register( 'ext.gadget.wefcore', 64773887, [ 'jquery.ui.autocomplete', 'jquery.ui.dialog', 'jquery.ui.datepicker', 'jquery.ui.tabs' ], undefined, 'ruwiki' );
-mw.loader.register( 'ext.gadget.wefflags', 64774333, undefined, undefined, 'ruwiki' );
 mw.loader.register( 'ext.gadget.wef-WorkEditor', 64774896, [ 'ext.gadget.wefcore', 'ext.gadget.wefflags', ], undefined, 'ruwiki' );
-
 mw.loader.using( 'ext.gadget.wef-WorkEditor' );
