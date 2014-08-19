13d63298f869ab7e55ba8c6acc9a3407bd29be4f
Sergey Vladimirov <vlsergey@gmail.com>
2014-08-19 16:42:39 +0400

    improve BSE search

--- gadget/Gadget-wefexternallinks.js
+++ gadget/Gadget-wefexternallinks.js
@@ -1233,7 +1233,7 @@ window.WEF_ExternalLinks = function() {
 			text: false,
 			label: 'Search on Yandex.Slovari website',
 			click: searchClickF( [ 'ruwiki', 'enwiki' ], function( title ) {
-				return '//slovari.yandex.ru/' + encodeURIComponent( title ) + '/%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5/';
+				return '//slovari.yandex.ru/' + encodeURIComponent( 'БСЭ ' + title ) + '/%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B5/';
 			} ),
 		} ],
 		qualifiers: [ d.P50, d.P357, d.P478 ],
