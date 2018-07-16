e6a946c22cff6c04df8efc1c2bd156abda916a89
Sergey Vladimirov <vlsergey@gmail.com>
2018-07-16 17:37:21 +0300

    [core/caches] cleanup code

--- src/caches/reducers.js
+++ src/caches/reducers.js
@@ -3,21 +3,7 @@ import expect from 'expect';
 
 const initialState = {
   cache: {},
-  queue: new Set(),
-  state: 'WAITING',
 };
-
-function add( set, cacheKeys ) {
-  const result = new Set( set );
-  result.add( cacheKeys );
-  return result;
-}
-
-function remove( set, cacheKeys ) {
-  const result = new Set( set );
-  cacheKeys.forEach( cacheKey => result.delete( cacheKey ) );
-  return result;
-}
 
 function cacheReducerBuilder( type ) {
   expect( type ).toBeA( 'string' );
@@ -32,18 +18,6 @@ function cacheReducerBuilder( type ) {
           ...state.cache,
           ...action.cacheUpdate,
         },
-      };
-    case 'CACHE_' + type + '_QUEUE':
-      expect( action.cacheKey ).toBeA( 'string' );
-      return {
-        ...state,
-        queue: add( state.queue, action.cacheKey ),
-      };
-    case 'CACHE_' + type + '_REMOVE_FROM_QUEUE':
-      expect( action.cacheKeys ).toBeAn( 'array' );
-      return {
-        ...state,
-        queue: remove( state.queue, action.cacheKeys ),
       };
     }
 
