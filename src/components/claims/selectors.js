300ab28bf40210b903c885258a39822ca2221b6d
Sergey Vladimirov <vlsergey@gmail.com>
2018-07-24 19:00:25 +0300

    [core] predefine sort for claims columns (make P580 always before P582 if both are shown)

--- src/components/claims/selectors.js
+++ src/components/claims/selectors.js
@@ -1,42 +1,58 @@
 import { defaultMemoize } from 'reselect';
+import stableSort from 'utils/stableSort';
 
 const ok = x => x !== undefined && x !== null;
 const EMPTY_ARRAY = [];
 
 const MAX_COLUMNS = 2;
 
-export const claimColumnsF = () => defaultMemoize( claims => {
-  if ( claims.length < 5 ) {
-    return EMPTY_ARRAY;
-  }
+const PREDEFINED_SORT = [
+  'P580', // start time
+  'P585', // point in time
+  'P582', // end time
+];
 
-  const qualifierStats = {};
-  claims
-    .map( claim => claim.qualifiers ).filter( ok )
-    .forEach( qualifiers => Object.keys( qualifiers )
-      .forEach( propertyId => {
-        const plus = ( qualifiers[ propertyId ] || EMPTY_ARRAY ).length > 0 ? 1 : 0;
-        qualifierStats[ propertyId ] = ( qualifierStats[ propertyId ] || 0 ) + plus;
-      } )
-    );
+export const claimColumnsF = () => {
 
-  const columns = [];
-  while ( columns.length < MAX_COLUMNS && Object.keys( qualifierStats ).length !== 0 ) {
-    const allValues = [ ...Object.values( qualifierStats ) ].sort( ( a, b ) => b - a );
-    const max = allValues[ 0 ];
-    if ( max < claims.length / 5.0 ) break;
+  const sortColumns = defaultMemoize( columns => stableSort( columns, ( c1, c2 ) => {
+    let o1 = PREDEFINED_SORT.indexOf( c1 );
+    o1 = o1 === -1 ? PREDEFINED_SORT.length : o1;
+    let o2 = PREDEFINED_SORT.indexOf( c2 );
+    o2 = o2 === -1 ? PREDEFINED_SORT.length : o2;
+    return o1 === o2 ? 0 : o1 > o2 ? +1 : -1;
+  } ) );
 
-    const countOfMax = allValues.filter( item => item === max ).length;
-    if ( columns.length + countOfMax > MAX_COLUMNS ) break;
+  return defaultMemoize( claims => {
+    if ( claims.length < 5 ) {
+      return EMPTY_ARRAY;
+    }
 
-    Object.keys( qualifierStats )
-      .filter( propertyId => qualifierStats[ propertyId ] === max )
-      .forEach( propertyId => {
-        columns.push( propertyId );
-        delete qualifierStats[ propertyId ];
-      } );
-  }
+    const qualifierStats = {};
+    claims
+      .map( claim => claim.qualifiers ).filter( ok )
+      .forEach( qualifiers => Object.keys( qualifiers )
+        .forEach( propertyId => {
+          const plus = ( qualifiers[ propertyId ] || EMPTY_ARRAY ).length > 0 ? 1 : 0;
+          qualifierStats[ propertyId ] = ( qualifierStats[ propertyId ] || 0 ) + plus;
+        } )
+      );
 
-  // const sorted = columns = columns.map( c => Number(c.substr(1)) ).sort().map( c=> 'P' + c);
-  return columns;
-} );
+    const columns = [];
+    while ( columns.length < MAX_COLUMNS && Object.keys( qualifierStats ).length !== 0 ) {
+      const allValues = [ ...Object.values( qualifierStats ) ].sort( ( a, b ) => b - a );
+      const max = allValues[ 0 ];
+      if ( max < claims.length / 5.0 ) break;
+
+      const countOfMax = allValues.filter( item => item === max ).length;
+      if ( columns.length + countOfMax > MAX_COLUMNS ) break;
+
+      Object.keys( qualifierStats )
+        .filter( propertyId => qualifierStats[ propertyId ] === max )
+        .forEach( propertyId => {
+          columns.push( propertyId );
+          delete qualifierStats[ propertyId ];
+        } );
+    }
+
+    return sortColumns( columns );
+  } );};
