e1f5884c2d99c87f71430f0e5fdc62169460a847
Sergey Vladimirov <vlsergey@gmail.com>
2018-12-25 13:18:04 +0300

    [sources] add search progress indication on source lookup tab

--- src/sources/SourceLookupTab.js
+++ src/sources/SourceLookupTab.js
@@ -17,6 +17,7 @@ export default class SourceLookupTab extends PureComponent {
     super( ...arguments );
 
     this.state = {
+      searchInProgress: false,
       searchTerm: '',
       searchTermScheduled: '',
       searchResult: [],
@@ -24,7 +25,9 @@ export default class SourceLookupTab extends PureComponent {
 
     this.handleChangeTerm = event => {
       const newSearchTerm = event.target.value || '';
-      this.setState( { searchTerm: newSearchTerm } );
+      this.setState( {
+        searchTerm: newSearchTerm,
+      } );
       setTimeout( this.search( newSearchTerm.trim() ), 0.5 );
     };
   }
@@ -32,36 +35,45 @@ export default class SourceLookupTab extends PureComponent {
   search( searchTerm ) {
     const { searchTermScheduled } = this.state;
     if ( searchTerm === searchTermScheduled ) return;
-    this.setState( { searchTermScheduled: searchTerm } );
+    this.setState( {
+      searchInProgress: true,
+      searchTermScheduled: searchTerm,
+    } );
 
-    if ( searchTerm === '' ) this.setState( { searchResult: [] } );
+    if ( searchTerm === '' ) this.setState( {
+      searchInProgress: false,
+      searchResult: [],
+    } );
 
     const result = [];
     const resultSet = new Set();
 
-    DEFAULT_LANGUAGES.forEach( languageCode => {
-      if ( result.size >= MAX_ITEMS ) return;
+    const wikidataApi = getWikidataApi();
+    const allPromises = DEFAULT_LANGUAGES.map( languageCode => wikidataApi.getPromise( {
+      action: 'wbsearchentities',
+      language: languageCode,
+      strictlanguage: false,
+      type: 'item',
+      limit: MAX_ITEMS,
+      search: searchTerm,
+    } ).then( ( { search } ) => {
+      search.forEach( ( { id } ) => {
+        if ( !resultSet.has( id ) ) {
+          result.push( id );
+          resultSet.add( id );
+        }
+      } );
 
-      getWikidataApi().get( {
-        action: 'wbsearchentities',
-        language: languageCode,
-        strictlanguage: false,
-        type: 'item',
-        limit: MAX_ITEMS,
-        search: searchTerm,
-      } ).done( ( { search } ) => {
-        search.forEach( ( { id } ) => {
-          if ( !resultSet.has( id ) ) {
-            result.push( id );
-            resultSet.add( id );
-          }
-        } );
+      if ( this.state.searchTermScheduled !== searchTerm ) return;
+      this.setState( { searchResult: result } );
+    }
+    ) );
 
-        if ( this.state.searchTermScheduled !== searchTerm ) return;
-        this.setState( { searchResult: result } );
-      }
-      );
-    } );
+    Promise.all( allPromises ).then(
+      () => this.setState( {
+        searchInProgress: false,
+      } )
+    );
   }
 
   handleClickF( entityId ) {
@@ -69,7 +81,7 @@ export default class SourceLookupTab extends PureComponent {
   }
 
   render() {
-    const { searchTerm, searchResult } = this.state;
+    const { searchTerm, searchInProgress, searchTermScheduled, searchResult } = this.state;
 
     return <div className={styles.sourceLookupTab}>
       <input
@@ -77,6 +89,13 @@ export default class SourceLookupTab extends PureComponent {
         onChange={this.handleChangeTerm}
         placeholder="Текст (название, описание) для поиска существующего источника в Викиданных"
         value={searchTerm} />
+      { searchInProgress
+        ? <div style={{ padding: '1em' }}>{'Идёт поиск источников по строке «' + searchTermScheduled + '»'}</div>
+        : searchResult.length !== 0
+          ? null
+          : searchTerm
+            ? <div style={{ padding: '1em' }}>{'Источников по строке «' + searchTermScheduled + '» не найдено'}</div>
+            : null}
       { searchResult.map( entityId =>
         <SourceItem
           entityId={entityId}
